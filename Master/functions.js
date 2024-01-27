ec49c6a8f4f2b38c86fc837a92cb59f95c61946c
Ramphys Tirana Brito <ramphytiranabrito@gmail.com>
2024-01-26 22:58:28 -0400

    proyecto validacion10 cambios extremos2

--- Master/functions.js
+++ Master/functions.js
@@ -11,3 +11,51 @@ ses.addEventListener('click', function(){
 
 // ( function (){
 // }) ();
+
+function play1(){
+    const http = new XMLHttpRequest();
+    const url = 'http://127.0.0.1:5500/Master/Calculator/index.html';
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+
+        document.getElementById('app0').innerHTML = this.responseText;  
+        }
+    }
+    http.open("GET", url, true);
+    http.send();
+}
+
+function play2(){
+    const http = new XMLHttpRequest();
+    const url = 'http://127.0.0.1:5500/Master/Chat/index.html';
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+
+        document.getElementById('app0').innerHTML = this.responseText;  
+        }
+    }
+    http.open("GET", url, true);
+    http.send();
+}
+
+ 
+//
+//
+/*
+ function peticion1(){
+    const http = new XMLHttpRequest();
+    const url = 'http://127.0.0.1:5500/htm/otro/index.html';
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+
+        document.getElementById('uno').innerHTML = this.responseText;  
+        }
+    }
+    http.open("GET", url, true);
+    http.send();
+}
+
+document.getElementById("gett1").addEventListener('click', function(){
+    peticion1();
+});
+*/
